df3ee814ef8ed8b23f89aa27ea85b8768a61ad38
dev-hannahk <dev.hannahk@gmail.com>
2023-02-12 21:54:55 +0900

    Chain

--- src/index.ts
+++ src/index.ts
@@ -23,3 +23,40 @@ class Block implements BlockShape {
     return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
+
+class Blockchain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+
+  private getPrevHash() {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+
+  public addBlock(data: string) {
+    const height = this.blocks.length;
+    const newBlock = new Block(this.getPrevHash(), height + 1, data);
+    this.blocks.push(newBlock);
+  }
+
+  // public getBlocks() {
+  //   return this.blocks;
+  // }
+
+  public getBlocks() {
+    return [...this.blocks];
+  }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("First One");
+blockchain.addBlock("Second One");
+blockchain.addBlock("Third One");
+blockchain.addBlock("Fourth One");
+
+blockchain.getBlocks().push(new Block("xxxx", 1111, "HACKED!!!!!"));
+
+console.log("getBlocks:::", blockchain.getBlocks());
